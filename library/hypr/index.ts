718ad0399d956f527e896a4e16b60bbf74c7231c
Nohavye <noha.poncelet@gmail.com>
2024-02-02 09:53:22 +0100

    Improved the definition of event ids. Using an enum provides the ability to iterate with ids.

--- library/hypr/index.ts
+++ library/hypr/index.ts
@@ -1,52 +1,31 @@
 import { hyprEventsHandler } from './HyprEvents'
 
-type EventId =
-    | 'workspace'
-    | 'focusedmon'
-    | 'activewindow'
-    | 'fullscreen'
-    | 'monitorremoved'
-    | 'monitoradded'
-    | 'createworkspace'
-    | 'destroyworkspace'
-    | 'moveworkspace'
-    | 'activelayout'
-    | 'openwindow'
-    | 'closewindow'
-    | 'movewindow'
-    | 'openlayer'
-    | 'closelayer'
-    | 'submap'
-    | 'changefloatingmode'
-    | 'urgent'
-    | 'minimize'
-    | 'screencast'
-    | 'windowtitle'
-
 export namespace Hypr {
-    const allEventIds: EventId[] = [
-        'workspace',
-        'focusedmon',
-        'activewindow',
-        'fullscreen',
-        'monitorremoved',
-        'monitoradded',
-        'createworkspace',
-        'destroyworkspace',
-        'moveworkspace',
-        'activelayout',
-        'openwindow',
-        'closewindow',
-        'movewindow',
-        'openlayer',
-        'closelayer',
-        'submap',
-        'changefloatingmode',
-        'urgent',
-        'minimize',
-        'screencast',
-        'windowtitle',
-    ]
+    export enum EventIds {
+        workspace = 'workspace',
+        focusedmon = 'focusedmon',
+        activewindow = 'activewindow',
+        fullscreen = 'fullscreen',
+        monitorremoved = 'monitorremoved',
+        monitoradded = 'monitoradded',
+        createworkspace = 'createworkspace',
+        destroyworkspace = 'destroyworkspace',
+        moveworkspace = 'moveworkspace',
+        activelayout = 'activelayout',
+        openwindow = 'openwindow',
+        closewindow = 'closewindow',
+        movewindow = 'movewindow',
+        openlayer = 'openlayer',
+        closelayer = 'closelayer',
+        submap = 'submap',
+        changefloatingmode = 'changefloatingmode',
+        urgent = 'urgent',
+        minimize = 'minimize',
+        screencast = 'screencast',
+        windowtitle = 'windowtitle',
+    }
+
+    type EventId = keyof typeof EventIds
 
     const eventListener = {
         add: (eventId: EventId, listener: (data: object) => any) => {
@@ -71,9 +50,9 @@ export namespace Hypr {
 
         if (typeof eventIds === 'string') {
             if (eventIds === 'all') {
-                allEventIds.forEach((eventId) =>
-                    handleListener(eventId, listener)
-                )
+                for (const eventId in EventIds) {
+                    handleListener(eventId as EventId, listener)
+                }
             } else {
                 handleListener(eventIds, listener)
             }
